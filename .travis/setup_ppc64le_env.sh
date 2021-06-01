7ceb91c07e19260352cf452b4b7028124b42ea2e
Abhijit-Mane <37098620+Abhijit-Mane@users.noreply.github.com>
2021-06-01 17:50:41 +0530

    testing travis.yml--8

--- .travis/setup_ppc64le_env.sh
+++ .travis/setup_ppc64le_env.sh
@@ -3,14 +3,15 @@
 set -ev
 
 # install needed packages
-sudo apt-get -y install build-essential gem make cmake ruby-dev
+sudo apt-get -y install build-essential gem make cmake
+#sudo apt-get -y install ruby-dev
 #sudo apt-get -y install zlib1g zlib1g-dev
 
 # setup bundler
 #sudo gem update --system
 
 #sudo gem install bundler
-sudo gem install bundler -v '2.1.4'
+#sudo gem install bundler -v '2.1.4'
 
 echo "Ruby, RubyGems version"
 sudo ruby --version 
@@ -18,3 +19,12 @@ sudo gem --version
 
 echo "Bundler declared in Gemfile.lock"
 sudo grep -A 1 "BUNDLED WITH" Gemfile.lock
+
+
+
+
+###############
+# The script below works
+sudo apt-get -y install build-essential gem make cmake
+sudo gem install bundler -v '2.1.4'
+###############
