32384a1ab86f9a5ee94c0bb746b9210c82072703
Abhijit-Mane <37098620+Abhijit-Mane@users.noreply.github.com>
2021-06-01 15:44:33 +0530

    testing travis.yml--4

--- .travis/setup_ppc64le_env.sh
+++ .travis/setup_ppc64le_env.sh
@@ -3,11 +3,12 @@
 set -ev
 
 # install needed packages
-sudo apt-get -y install build-essential gem make cmake ruby-dev zlib.ppc64le zlib-devel.ppc64le
+apt-get -y install build-essential gem make cmake ruby-dev
+#apt-get -y install zlib1g zlib1g-dev
 
 # setup bundler
-sudo gem update --system
-sudo gem uninstall bundler
-sudo rm /usr/local/bin/bundle  || true
-sudo rm /usr/local/bin/bundler || true
-sudo gem install bundler
+gem update --system
+gem uninstall bundler
+rm /usr/local/bin/bundle  || true
+rm /usr/local/bin/bundler || true
+gem install bundler
